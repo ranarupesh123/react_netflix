04472689ad89589e6bc8af3721161bd2167566cb
ranarupesh <rupeshrana288@gamil.com>
2020-07-15 21:10:50 +0530

    first

---
+++ src/sdata.jsx
@@ -0,0 +1,45 @@
+const Sdata = [
+  {
+    id:1,
+    sname : "DARK",
+    imgsrc : "https://wallpapercave.com/wp/wp4056410.jpg",
+    title : "A Netflix original series",
+    link : "https://www.netflix.com/in/title/80100172"
+  },
+  {
+    id:2,
+    sname : "STRANGER THINGS",
+    imgsrc : "https://wallpapercave.com/wp/wp1839578.jpg",
+    title : "A Netflix original series",
+    link : "https://www.netflix.com/in/title/80057281"
+  },
+  {
+    id:3,
+    sname : "PUNISHER",
+    imgsrc : "https://wallpapercave.com/wp/wp5475742.jpg",
+    title : "A Netflix original series",
+    link : "https://www.netflix.com/in/title/80117498"
+  },
+  {
+    id:4,
+    sname : "NARCOS",
+    imgsrc : "https://wallpapercave.com/wp/wp6547537.jpg",
+    title : "A Netflix original series",
+    link : "https://www.netflix.com/in/title/80025172"
+  },
+  {
+    id:5,
+    sname : "LA CASA DE PAPEL",
+    imgsrc : "https://wallpapercave.com/wp/wp6370470.jpg",
+    title : "A Netflix original series",
+    link : "https://www.netflix.com/in/title/80192098"
+  },
+  {
+    id:6,
+    sname : "SEX EDUCATION",
+    imgsrc : "https://wallpapercave.com/wp/wp5152709.png",
+    title : "A Netflix original series",
+    link : "https://www.netflix.com/in/title/80197526"
+  },
+];
+export default Sdata;
